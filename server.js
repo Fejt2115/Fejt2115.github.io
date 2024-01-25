1809e989dcc7d85279aa70bdc53099e750c239ce
Fejt2115 <mszoltyski@gmail.com>
2024-01-25 19:17:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,15 +19,9 @@ app.use(bodyParser.json());
 function getCalculatedValue(workbook, sheetName, cellAddress) {
     const sheet = workbook.Sheets[sheetName];
     const cell = sheet[cellAddress];
-
-    if (cell && cell.f) {
-        const formula = cell.f;
-        const result = parser.parse(formula);
-        return result.result;
-    } else {
-        console.error(`Formula not found for cell ${cellAddress}`);
-        return null; // You can handle this case based on your requirements
-    }
+    const formula = cell.f;
+    const result = parser.parse(formula);
+    return result.result;
 }
 
 function setValueToCell(workbook, sheetName, cellAddress, value) {
